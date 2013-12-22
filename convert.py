be336122f1c538d843013fba9267abb250b405ee
LexWraith <lex.wraith@gmail.com>
2013-12-21 19:02:35 -0500

    Halfway...

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 def openFile(filename):
     """
@@ -16,17 +16,23 @@ def openFile(filename):
                     words.append(elem)
         return words
 
-def randomSpacing(words,chance=percentage):
+def randomChunking(words,percentage=0):
     """
-    Cummings had a thing for "cubism" poetry and would often randomly
-    remove spacings between words. 
+    Randomly removes spaces between words.
+    IT TURNS THE WORD ARRAY INTO A WORDS LIST,
+    I.E. A LIST OF CHUNKS.
+    
+    TODO: Kind of memory intensive because of the rapid
+    list re-copying. Need to find a better way to do this.
+    
+    IDEA: Couldn't this be done recursively? The sub-problem
+    is whether or not the 0th word and the 1st word should be
+    chunked together.
     """
-    #Making sure percentage is in range of 0-100
-    if 0 < percentage < 1:
-        percentage *= 100
-    
-    #The actual removing of spaces.
-    
+    chunks = []
+    while len(words) > 0:
+        roll = randint(1,99)
+        if 
 
 def randomNewLine(words):
     """
@@ -38,14 +44,47 @@ def randomTabbing(words):
     Cummings also would randomize the tabbing.
     """
 
-def randomCapitalization(words):
+def randomCapitalization(words,percentage=0):
     """
-    Determines 
+    Capitalizes the first letter or not.
     """
-    pass
-
+    newWords = []
+    for elem in words:
+        if randint(1,99) < percentage and elem not in ".!,;:?":
+            newWords.append(elem.capitalize())
+        else:
+            newWords.append(elem.lower())
+    return newWords
+        
+def randomCase(words,percentageWord=0,percentageLetter=0):
+    """
+    Random capitalization of all letters within the word list.
+    """
+    newWords = []
+    for word in words:
+        if  randint(1,99) < percentageWord:
+            temp = [letter.upper() if randint(1,99) < percentageLetter else letter for letter in word]
+            newWords.append("".join(temp))
+        else:
+            newWords.append(word)
+    return newWords
+        
 def main(filename):
+    """
+    Flow of operations:
+    1. Open file and get word list
+    2. Random capitalization of first letter
+    3. Random capitalization of rest of word
+    4. "Chunking" words together
+    5. Randomizing what chunks are independent lines
+    6. Randomizing tabbing of each line
+    7. Output into screen or file
+    """
     words = openFile("sample.txt")
+    words = randomCapitalization(words,10)
+    words = randomCase(words,20,20)
+    words = randomChunk(words,20)
+    print words
     
 
 if __name__ == "__main__":
